ad9fd1ce3b63839e96585ef6849ded639c27d916
marianaplazas <mariana.plazas@mercadolibre.com.co>
2022-09-04 18:28:24 -0500

    add buttons

--- sketch.js
+++ sketch.js
@@ -3,37 +3,59 @@ let width = 1300; //Viene del html
 let height = 930; 
 let sR = width/nRC;// ancho de cada celda
 let sC = width/nRC; // altura de cada celda
-let grid =initArray()
+let grid = initArray()
 let canvas,ctx;
-grid = randomArray(grid);
-setInterval(game,100);
-function game(){
-canvas = document.getElementById('gameCanvas');
-ctx = canvas.getContext('2d');
-ctx.fillStyle = 'black';
-ctx.fillRect(0,0,canvas.width,canvas.height) // Rellenar la pantalla con negro para que no se acumulen frames
-for (let y = 0; y < nRC; y++){
-  for (let x = 0; x < nRC; x++){
-    ctx.strokeStyle = '#f8f8f8';
-    ctx.strokeRect(x*sR,y*sC,sR,sC)
-    if (grid[x][y] == 1){
-      ctx.fillStyle = '#f8f8f8'
+let interval;
+
+function paintCanvas(){
+  for (let y = 0; y < nRC; y++){
+    for (let x = 0; x < nRC; x++){
+      ctx.strokeStyle = '#f8f8f8';
+      ctx.strokeRect(x*sR,y*sC,sR,sC)
+      ctx.fillStyle = '#6B8E23'
       ctx.fillRect(x*sR,y*sC,sR,sC)
-    }}} 
-  
-let nextGrid = initArray();
-for (let i =0; i < nRC; i++){
-  for (let j= 0; j< nRC; j++){
-    let state = grid[i][j];
-    let neighbours = livingCells(grid,i,j);
-    nextGrid[i][j] = rules(state,neighbours,i,j)
+      if (grid[x][y] == 1){
+        ctx.fillStyle = '#f8f8f8'
+        ctx.fillRect(x*sR,y*sC,sR,sC)
+      }}}
+}
+
+function cleanCanvas(){
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0,0,canvas.width,canvas.height) // Rellenar la pantalla con negro para que no se acumulen frames
+  for (let y = 0; y < nRC; y++){
+    for (let x = 0; x < nRC; x++){
+      ctx.strokeStyle = '#f8f8f8';
+      ctx.strokeRect(x*sR,y*sC,sR,sC);
+      ctx.fillStyle = '#6B8E23';
+      ctx.fillRect(x*sR,y*sC,sR,sC);
+    }}
+}
+
+function randomCanvas(){
+  ctx.fillRect(0,0,canvas.width,canvas.height) // Rellenar la pantalla con negro para que no se acumulen frames
+  grid = randomArray(grid);
+  paintCanvas();   
+}
+
+function startGame(){
+  let nextGrid = initArray();
+  for (let i = 0; i < nRC; i++){
+    for (let j= 0; j< nRC; j++){
+      let state = grid[i][j];
+      let neighbours = livingCells(grid,i,j);
+      nextGrid[i][j] = rules(state,neighbours,i,j)
+    }
   }
-
+  grid = nextGrid;
+  paintCanvas();
 }
-grid = nextGrid;
-
+function startGameInterval(){
+  interval = setInterval(startGame, 100);
 }
-
+function stop(){
+  clearInterval(interval);
+}
 function initArray(){
 let array = Array(nRC)
   for (let i=0;i<nRC; i++){
@@ -71,4 +93,7 @@ function rules(state,n,i,j){
     else {
       return state;
     }}
-document.addEventListener('DOMContentLoaded', game)
+
+canvas = document.getElementById('gameCanvas');
+ctx = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', cleanCanvas)
