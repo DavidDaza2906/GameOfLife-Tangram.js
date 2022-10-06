07975e80f6a20c2744190e48a52bdc55e249a1cf
David <ddazaj@unal.edu.co>
2022-10-05 19:00:45 -0500

    bug fixes

--- Tangram/script.js
+++ Tangram/script.js
@@ -3,57 +3,75 @@ let ctx = canvas.getContext('2d');
 let nRC = 10;
 let sR = canvas.height/nRC;
 let sC = canvas.width/nRC;
+let coor= []
+function distance(x1,y1,x2,y2){
+   return (Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)))
+}
 
-let polygons = {
-  constructor(sides,color,x,y,sideLength,rotation,name,regular){
-    this.sides = sides;
+
+let rectangle = {
+  constructor(color,x,y,sideLength,rotation,name){
     this.color = color;
     this.x = x;
     this.y = y;
     this.sideLength = sideLength;
     this.rotation = rotation;
     this.name = name;
-    this.regular = regular;
   },
   draw(){
     this.name = new Path2D();
-    const a = ((Math.PI * 2)/ this.sides);
-    if (this.regular){
-    for (let i = 1; i < this.sides + 1; i++) {
-      this.name.lineTo(this.x + this.sideLength * Math.cos(a*i+this.rotation), this.y + this.sideLength * Math.sin(a*i+this.rotation));
-  }}
-    else{ 
-        this.name.lineTo(this.x + this.sideLength* Math.cos(a*1+this.rotation), this.y + this.sideLength* Math.sin(a*1+this.rotation));
-        this.name.lineTo(this.x + this.sideLength* Math.cos(a*2+this.rotation), this.y + this.sideLength* Math.sin(a*2+this.rotation))
-        this.name.lineTo(this.x + this.sideLength* Math.cos(a*3+this.rotation),this.y + this.sideLength * Math.sin(a+this.rotation))
-        this.name.lineTo(this.x + this.sideLength* Math.cos(a*4+this.rotation),this.y + this.sideLength * Math.sin(a*4+this.rotation))
-      }
+    const a = ((Math.PI * 2)/ 4);
+    for (let i = 0; i < 4 + 0; i++) {
+      this.name.lineTo(this.x + (this.sideLength * Math.cos(a*i+this.rotation)), this.y + (this.sideLength * Math.sin(a*i+this.rotation)));
+    }
   ctx.fillStyle = this.color;
   ctx.fill(this.name);
   }
 };
 
-let rectangle = Object.create(polygons);
-rectangle.sides = 4;
+let triangle = { 
+  constructor(color,x,y,sideLength,rotation,name){
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.sideLength = sideLength;
+    this.rotation = rotation;
+    this.name = name;
+},
+  draw(){
+    /// Crear triangulo en base al centro
+    ctx.lineWidth = 4;
+    this.name = new Path2D();
+    this.name.moveTo(this.x, this.y);
+    this.name.lineTo()
+    this.name.lineTo(this.x+this.sideLength,this.y);
+    this.name.lineTo(this.x+this.sideLength*Math.cos(Math.PI/3),this.y+this.sideLength*Math.sin(Math.PI/3))
+    this.name.lineTo(this.x,this.y)
+    ctx.fillStyle = this.color;
+    ctx.fill(this.name)
+  }
+}
+
+
+let firstRectangle = Object.create(rectangle);
 rectangle.color = "rgba(46,142,222,1)";
 rectangle.x = 150;
 rectangle.y = 300;
 rectangle.sideLength = 120;
 rectangle.rotation = Math.PI/4;
 rectangle.name = rectangle;
-rectangle.regular = true;
 
-let smallTriangle1 = Object.create(polygons);
+let smallTriangle1 = Object.create(triangle);
 smallTriangle1.sides = 3;
 smallTriangle1.color = "rgba(241,91,96,1)";
-smallTriangle1.x = 150;
-smallTriangle1.y = 110;
+smallTriangle1.x = 100;
+smallTriangle1.y = 100;
 smallTriangle1.sideLength = 100;
 smallTriangle1.rotation = Math.PI/6;
 smallTriangle1.name = smallTriangle1;
 smallTriangle1.regular = true;
 
-let smallTriangle2 = Object.create(polygons);
+let smallTriangle2 = Object.create(triangle);
 smallTriangle2.sides = 3;
 smallTriangle2.color = "rgba(57,181,160,1)";
 smallTriangle2.x = 150;
@@ -64,28 +82,28 @@ smallTriangle2.name = smallTriangle2;
 smallTriangle2.regular = true;
 
 
-let bigTriangle1 = Object.create(polygons);
+let bigTriangle1 = Object.create(triangle);
 bigTriangle1.sides = 3;
 bigTriangle1.color = "rgba(164, 146, 234,1)";
-bigTriangle1.x = 200;
-bigTriangle1.y = 800;
+bigTriangle1.x = 400;
+bigTriangle1.y = 600;
 bigTriangle1.sideLength = 200;
-bigTriangle1.rotation = Math.PI/6;
+bigTriangle1.rotation = Math.PI/2;
 bigTriangle1.name = bigTriangle1;
 bigTriangle1.regular = true;
 
 
-let bigTriangle2 = Object.create(polygons);
+let bigTriangle2 = Object.create(triangle);
 bigTriangle2.sides = 3;
 bigTriangle2.color = "rgba(193, 212, 94,1)";
 bigTriangle2.x = 500;
 bigTriangle2.y = 200;
 bigTriangle2.sideLength = 200;
-bigTriangle2.rotation = Math.PI/6;
+bigTriangle2.rotation = Math.PI;
 bigTriangle2.name = bigTriangle2;
 bigTriangle2.regular = true;
 
-let rightTriangle = Object.create(polygons);
+let rightTriangle = Object.create(triangle);
 rightTriangle.sides = 3;
 rightTriangle.color = "rgba(241,91,96,1)";
 rightTriangle.x = 650;
@@ -120,11 +138,13 @@ function draw(){
   ctx.fill();
   grid();
   smallTriangle1.draw();
-  rectangle.draw();
-  smallTriangle2.draw();
-  bigTriangle1.draw();
-  bigTriangle2.draw();
-  rightTriangle.draw();
+  //rectangle.draw();
+  //smallTriangle2.draw();
+  //bigTriangle1.draw();
+  //bigTriangle2.draw();
+  //rightTriangle.draw();
+  //triangle.draw();
+  firstRectangle.draw();
 }
 
 
@@ -139,17 +159,17 @@ canvas.addEventListener("mousedown", event => {
   x= Math.floor(((event.clientX - rect.left) / (rect.right - rect.left) * canvas.width));
   y= Math.floor(((event.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height));
   figuresTotal.push(smallTriangle1);
-  figuresTotal.push(rectangle);
-  figuresTotal.push(smallTriangle2);
-  figuresTotal.push(bigTriangle1);
-  figuresTotal.push(bigTriangle2);
-  figuresTotal.push(rightTriangle)
-
+  figuresTotal.push(firstRectangle);
+  //figuresTotal.push(smallTriangle2);
+  //figuresTotal.push(bigTriangle1);
+  //figuresTotal.push(bigTriangle2);
+  //figuresTotal.push(rightTriangle)
+  console.log(x,y)
 
   for (let i = 0; i < figuresTotal.length; i++){
     isPointInPath = ctx.isPointInPath(figuresTotal[i].name, x, y);
     if (isPointInPath){
-      figuresTotal[i].rotation += Math.PI/6;
+      figuresTotal[i].rotation += Math.PI/12;
       down = true;
     }
   }
