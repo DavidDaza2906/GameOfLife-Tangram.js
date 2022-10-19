582003f1bca3fd5ceb1e633ec6dc006f193fb6df
David <ddazaj@unal.edu.co>
2022-10-18 21:53:52 -0500

    paralelogramo reflexion

--- Tangram/script.js
+++ Tangram/script.js
@@ -1,11 +1,12 @@
 class polygon{
-  constructor(color,x,y,sideLength,rotation,name){
+  constructor(color,x,y,sideLength,rotation,name,reversed){
     this.color = color;
     this.x = x;
     this.y = y;
     this.sideLength = sideLength;
     this.rotation = rotation;
     this.name = name;
+    this.reversed = reversed;
   }
 }
 
@@ -39,6 +40,7 @@ class triangle extends polygon {
 
 class parallelogram extends polygon{
     draw(){
+      if (!this.reversed){
         let x2 = this.x + (this.sideLength*Math.cos((Math.PI/2) + this.rotation));
         let y2 = this.y + (this.sideLength * Math.sin((Math.PI/2) + this.rotation));
         this.name = new Path2D();
@@ -49,12 +51,30 @@ class parallelogram extends polygon{
         this.name.lineTo(this.x, this.y);
         ctx.fillStyle = this.color;
         ctx.fill(this.name);
-        ctx.lineWidth = 4
-        ctx.strokeStyle = "white"
-        ctx.stroke(this.name)
-
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = "white";
+        ctx.stroke(this.name);}
+    
+    else{
+      let x1 = this.x + (this.sideLength*Math.cos(Math.PI*2 + this.rotation));
+      let y1 = this.y + (this.sideLength * Math.sin(Math.PI*2 + this.rotation));
+      let x2 = this.x + (this.sideLength*Math.cos((Math.PI/2 + this.rotation)));
+      let y2 =this.y + (this.sideLength * Math.sin((Math.PI/2 + this.rotation) ));
+      this.name = new Path2D();
+      this.name.moveTo(this.x, this.y);
+      this.name.lineTo(x1,y1);
+      this.name.lineTo(x2, y2);
+      this.name.lineTo(x2 + (this.sideLength*Math.cos((Math.PI) + this.rotation)), y2 + (this.sideLength * Math.sin((Math.PI) + this.rotation)));
+      this.name.lineTo(this.x, this.y);
+      ctx.fillStyle = this.color;
+      ctx.fill(this.name);
+      ctx.lineWidth = 4;
+      ctx.strokeStyle = "white";
+      ctx.stroke(this.name);}
+  
     }
 }
+
 
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext('2d');
@@ -69,7 +89,7 @@ let smallTriangle2 = new triangle("rgba(57,181,160,1)",600,600,280,Math.PI/4, 't
 let bigTriangle1 = new triangle("rgba(164, 146, 234,1)",600,600,560,5*Math.PI/4,'triangle')
 let bigTriangle2 = new triangle("rgba(193, 212, 94,1)",600,600,560,3*Math.PI/4,'triangle')
 let midTriangle = new triangle("rgba(150,1,16,1)",1000,1000, 400,Math.PI, 'triangle')
-let parallelogram1 = new parallelogram("rgba(241,100,260,1)",400,800,280,7*Math.PI/4,'parallelogram')
+let parallelogram1 = new parallelogram("rgba(241,100,260,1)",400,500,280,0,'parallelogram',false)
 let figuresTotal = [];
 figuresTotal.push(smallTriangle1);
 figuresTotal.push(firstRectangle);
@@ -116,6 +136,7 @@ canvas.addEventListener("mousemove", event => {
     }
 })
 canvas.addEventListener("mousedown", event =>{
+  console.log(x,y);
     down = true;
     if (event.shiftKey){
         for (let i = 0; i, figuresTotal.length; i++){
@@ -125,7 +146,14 @@ canvas.addEventListener("mousedown", event =>{
             }
         }
     }
-})
+    else if (event.ctrlKey){
+      for (let i = 0; i, figuresTotal.length; i++){
+        isPointInPath = ctx.isPointInPath(figuresTotal[i].name,x,y)
+        if (isPointInPath){
+            figuresTotal[i].reversed = !figuresTotal[i].reversed;
+
+    }
+}}})
 canvas.addEventListener("mouseup", event =>{
     down = false;
 })
---
+++ xd.js
@@ -0,0 +1,5 @@
+function sum(a, b=2, c=3){
+    return a + b + c
+}
+let values = [1,5]
+console.log(sum(4,...values))
